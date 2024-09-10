c945070723d714829c76d4b4ab9f88f44b8d60b9
aavetisyanIT <aavetisyanIT@gmail.com>
2024-09-10 17:09:53 -0500

    Mount middleware to work with requests

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,10 @@
 import express, { Application, Request, Response } from "express";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`Hello, TypeScript with Express!`);
