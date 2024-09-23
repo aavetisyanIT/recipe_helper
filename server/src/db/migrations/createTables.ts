e9493a90f0378d41d33cd345e48bc0ed41ae619e
aavetisyanIT <aavetisyanIT@gmail.com>
2024-09-23 17:53:53 -0500

    Remove redundant imports

--- server/src/db/migrations/createTables.ts
+++ server/src/db/migrations/createTables.ts
@@ -1,5 +1,3 @@
-import "dotenv/config";
-
 export const createTables = `
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
--- server/src/db/migrations/grantPermissions.ts
+++ server/src/db/migrations/grantPermissions.ts
@@ -1,5 +1,3 @@
-import "dotenv/config";
-
 export const grantUserPermissions = `
   GRANT CREATE ON SCHEMA public TO ${process.env.DB_USER};
   GRANT USAGE ON SCHEMA public TO ${process.env.DB_USER};
