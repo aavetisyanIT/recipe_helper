1b0581b828a806b88e3d935fc758cad893221504
aavetisyanIT <aavetisyanIT@gmail.com>
2024-09-24 12:02:44 -0500

    Conditionally use https or http protocols and ports based on environment

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,37 @@
+import http from "http";
+import https from "https";
+import fs from "fs";
+import path from "path";
 import dotenv from "dotenv";
+
 import app from "./app";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
 
-const PORT = process.env.HTTP_PORT;
+const HTTP_PORT = process.env.HTTP_PORT || 80;
+const HTTPS_PORT = process.env.HTTPS_PORT || 443;
 
-app.listen(PORT, () =>
-  console.log(`Recipe Server is running on port: ${PORT}`),
-);
+const isHttps = process.env.USE_HTTPS === "true";
+
+if (isHttps) {
+  const projectRoot = path.dirname(__dirname);
+  const key = fs.readFileSync(
+    path.resolve(projectRoot, process.env.SSL_KEY_PATH || "./certs/server.key"),
+  );
+  const cert = fs.readFileSync(
+    path.resolve(
+      projectRoot,
+      process.env.SSL_CERT_PATH || "./certs/server.cert",
+    ),
+  );
+  const options = { key, cert };
+  const httpsServer = https.createServer(options, app);
+  httpsServer.listen(HTTPS_PORT, () => {
+    console.log(`HTTPS Recipe Server is running on port: ${HTTPS_PORT}`);
+  });
+} else {
+  const httpServer = http.createServer(app);
+  httpServer.listen(HTTP_PORT, () =>
+    console.log(`HTTP Recipe Server is running on port: ${HTTP_PORT}`),
+  );
+}
