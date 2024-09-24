ac224643ed8f16db94deae1d620f85c51da9903a
aavetisyanIT <aavetisyanIT@gmail.com>
2024-09-24 12:48:30 -0500

    Mount rateLimiter middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,16 +5,16 @@ import cookieParser from "cookie-parser";
 import { pool } from "./config/database.connection";
 import authRouter from "./routes/authRouter";
 import { IUser } from "./models";
-import { requireAuth } from "./middleware";
+import { requireAuth, rateLimiter } from "./middleware";
 
 const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/auth", authRouter);
+app.use("/auth", rateLimiter, authRouter);
 
-app.get("/", requireAuth, async (req: Request, res: Response) => {
+app.get("/", rateLimiter, requireAuth, async (req: Request, res: Response) => {
   const result: QueryResult<IUser> = await pool.query("SELECT * FROM users");
   res.status(200).json(result.rows);
 });
