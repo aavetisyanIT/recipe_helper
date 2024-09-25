f19add1943079cef70b98c26d122f7e307cf3253
aavetisyanIT <aavetisyanIT@gmail.com>
2024-09-25 11:31:34 -0500

    Mount user login validation and error handling middlewares

--- server/src/routes/authRouter.ts
+++ server/src/routes/authRouter.ts
@@ -1,11 +1,21 @@
 import express, { Router } from "express";
 
 import { register_post, login_post, logout_get } from "../controllers";
+import {
+  userLoginValidation,
+  userRegistrationValidation,
+} from "../middleware/validators";
+import { handleAuthErrors } from "../middleware";
 
 const authRouter: Router = express.Router();
 
-authRouter.post("/register", register_post);
-authRouter.post("/login", login_post);
+authRouter.post(
+  "/register",
+  userRegistrationValidation,
+  handleAuthErrors,
+  register_post,
+);
+authRouter.post("/login", userLoginValidation, handleAuthErrors, login_post);
 authRouter.get("/logout", logout_get);
 
 export default authRouter;
