3ac8151a1ba321579e10f795fead00082dc6c6ef
aavetisyanIT <aavetisyanIT@gmail.com>
2024-09-25 16:54:07 -0500

    Add caching logic for requireAuth middleware

--- server/src/middleware/requireAuth.ts
+++ server/src/middleware/requireAuth.ts
@@ -2,17 +2,43 @@ import { verify, VerifyErrors } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 import "dotenv/config";
 
-export function requireAuth(req: Request, res: Response, next: NextFunction) {
-  const jwt = req.cookies.auth_token;
+import { redisClient } from "../config";
 
-  if (!jwt) {
+export async function requireAuth(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const authToken = req.cookies.auth_token;
+
+  if (!authToken) {
     return res.status(403).json({ error: "User is not logged in" });
   }
+  //check if token is cached in Redis under key: `jwt:${token}`
+  const cachedToken = await redisClient.get(`jwt:${authToken}`);
+  if (cachedToken) {
+    return next();
+  }
+  verify(
+    authToken,
+    process.env.JWT_SECRET as string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (err: VerifyErrors | null, decoded: any) => {
+      if (err) {
+        return res.status(403).json({ error: "User is not logged in" });
+      }
+      // if token not cached verify token authToken.verify
+      // Cache the decoded token for the remaining token validity period (exp - current time)
+      const expiryTime = decoded.exp
+        ? decoded.exp - Math.floor(Date.now() / 1000)
+        : Number(process.env.MAX_TOKEN_AGE);
 
-  verify(jwt, process.env.JWT_SECRET as string, (err: VerifyErrors | null) => {
-    if (err) {
-      return res.status(403).json({ error: "User is not logged in" });
-    }
-    next();
-  });
+      redisClient.setEx(
+        `jwt:${authToken}`,
+        expiryTime,
+        JSON.stringify(decoded),
+      );
+      next();
+    },
+  );
 }
