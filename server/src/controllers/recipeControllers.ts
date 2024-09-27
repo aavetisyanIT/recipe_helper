c9508dee772c326197e522d985f05f5e1871298b
aavetisyanIT <aavetisyanIT@gmail.com>
2024-09-27 13:03:12 -0500

    Create GET and POST recipe controllers

--- server/src/controllers/recipeControllers.ts
+++ server/src/controllers/recipeControllers.ts
@@ -1,10 +1,23 @@
 import { Response } from "express";
 
 import { ICreateRecipeRequest } from "../types/recipe.types";
+import { IAuthUserRequest } from "../types";
+
+export const recipes_get = (req: IAuthUserRequest, res: Response) => {
+  const { user } = req;
+  if (!user) {
+    return res.status(403).json({ error: "User not authenticated" });
+  }
+
+  res.status(200).send(`get recipe for user ${user.userName}`);
+};
 
 export const recipe_post = (req: ICreateRecipeRequest, res: Response) => {
-  const { userId } = req.body;
-  console.log("recipe post", userId);
+  const { user } = req;
 
-  res.status(200).send("test");
+  if (!user) {
+    return res.status(403).json({ error: "User not authenticated" });
+  }
+
+  res.status(200).send(`created recipe for user ${user.userName}`);
 };
--- server/src/routes/recipeRouter.ts
+++ server/src/routes/recipeRouter.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
 
-import { recipe_post } from "../controllers";
+import { recipe_post, recipes_get } from "../controllers";
 
 const recipeRouter: Router = express.Router();
 
+recipeRouter.get("/", recipes_get);
 recipeRouter.post("/", recipe_post);
 
 export default recipeRouter;
