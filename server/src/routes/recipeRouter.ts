002e68d0c4627c8c4d83cfb4bc3f1714913447f4
aavetisyanIT <aavetisyanIT@gmail.com>
2024-09-27 15:31:53 -0500

    Mount recipe validation middleware

--- server/src/routes/recipeRouter.ts
+++ server/src/routes/recipeRouter.ts
@@ -2,10 +2,16 @@ import express, { Router } from "express";
 
 import { recipe_post, recipes_get } from "../controllers";
 import { recipeCreationValidation } from "../middleware/validators";
+import { handleRecipeErrors } from "../middleware/handleRecipeErrors";
 
 const recipeRouter: Router = express.Router();
 
 recipeRouter.get("/", recipes_get);
-recipeRouter.post("/", recipeCreationValidation, recipe_post);
+recipeRouter.post(
+  "/",
+  recipeCreationValidation,
+  handleRecipeErrors,
+  recipe_post,
+);
 
 export default recipeRouter;
