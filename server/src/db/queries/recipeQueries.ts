2e5c3f0dffe825b9aed99103ca39e8bbc37317dd
aavetisyanIT <aavetisyanIT@gmail.com>
2024-09-27 15:07:04 -0500

    Create recipe queries to get and post recipes

---
+++ server/src/db/queries/recipeQueries.ts
@@ -0,0 +1,24 @@
+import { QueryConfig } from "pg";
+
+export function selectAllRecipesByUserId(id: number): QueryConfig {
+  return {
+    text: `SELECT * FROM recipes WHERE "author_id" = $1`,
+    values: [id],
+  };
+}
+
+export function createRecipe(
+  title: string,
+  ingredients: string[],
+  instructions: string[],
+  author_id: number,
+): QueryConfig {
+  return {
+    text: `
+        INSERT INTO recipes (title, ingredients, instructions, author_id)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id, title, ingredients, instructions, author_id, created_at;
+      `,
+    values: [title, ingredients, instructions, author_id],
+  };
+}
