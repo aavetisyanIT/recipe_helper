791fbdf6ba5e22f86ce13ba26c6fced62cb037f3
aavetisyanIT <aavetisyanIT@gmail.com>
2024-09-27 15:07:31 -0500

    Add logic to recipe controllers

--- server/src/controllers/recipeControllers.ts
+++ server/src/controllers/recipeControllers.ts
@@ -1,23 +1,63 @@
 import { Response } from "express";
 
 import { ICreateRecipeRequest } from "../types/recipe.types";
-import { IAuthUserRequest } from "../types";
+import { IAuthUserRequest, IErrorResponse } from "../types";
+import { createRecipe, selectAllRecipesByUserId } from "../db";
+import { IRecipe } from "../models";
+import { pool } from "../config";
+import { QueryResult } from "pg";
 
-export const recipes_get = (req: IAuthUserRequest, res: Response) => {
+export const recipes_get = async (
+  req: IAuthUserRequest,
+  res: Response<IRecipe[] | IErrorResponse>,
+) => {
   const { user } = req;
   if (!user) {
     return res.status(403).json({ error: "User not authenticated" });
   }
 
-  res.status(200).send(`get recipe for user ${user.userName}`);
+  try {
+    const userRecipes: QueryResult<IRecipe> = await pool.query(
+      selectAllRecipesByUserId(user.id),
+    );
+    if (userRecipes.rowCount === 0) {
+      return res.status(400).json({ error: "This user does not have recipes" });
+    }
+
+    res.status(200).send(userRecipes.rows);
+  } catch (err) {
+    console.error(err);
+    const errorResponse: IErrorResponse = {
+      error: "Not able to find recipes for this user",
+    };
+    res.status(500).json(errorResponse);
+  }
 };
 
-export const recipe_post = (req: ICreateRecipeRequest, res: Response) => {
+export const recipe_post = async (
+  req: ICreateRecipeRequest,
+  res: Response<IRecipe | IErrorResponse>,
+) => {
   const { user } = req;
+  const { title, ingredients, instructions } = req.body;
 
   if (!user) {
     return res.status(403).json({ error: "User not authenticated" });
   }
 
-  res.status(200).send(`created recipe for user ${user.userName}`);
+  try {
+    const newRecipe: QueryResult<IRecipe> = await pool.query(
+      createRecipe(title, ingredients, instructions, user.id),
+    );
+
+    const recipe = newRecipe.rows[0];
+
+    res.status(200).send(recipe);
+  } catch (err) {
+    console.error(err);
+    const errorResponse: IErrorResponse = {
+      error: "Not able to create recipes for this user",
+    };
+    res.status(500).json(errorResponse);
+  }
 };
--- server/src/routes/recipeRouter.ts
+++ server/src/routes/recipeRouter.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
 
 import { recipe_post, recipes_get } from "../controllers";
+import { recipeCreationValidation } from "../middleware/validators";
 
 const recipeRouter: Router = express.Router();
 
 recipeRouter.get("/", recipes_get);
-recipeRouter.post("/", recipe_post);
+recipeRouter.post("/", recipeCreationValidation, recipe_post);
 
 export default recipeRouter;
