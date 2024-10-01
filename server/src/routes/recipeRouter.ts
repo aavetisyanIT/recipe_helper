90260eac7e67cf297d7e235c8e0c7a84fdeae77f
aavetisyanIT <aavetisyanIT@gmail.com>
2024-10-01 16:33:46 -0500

    Mount and test recipe controller class

--- server/src/routes/recipeRouter.ts
+++ server/src/routes/recipeRouter.ts
@@ -1,18 +1,18 @@
 import express, { Router } from "express";
 
-import { recipe_by_id_get, recipe_post, recipes_get } from "../controllers";
+import { controller } from "../controllers";
 import { recipeCreationValidation } from "../middleware/validators";
 import { handleRecipeErrors } from "../middleware/handleRecipeErrors";
 
 const recipeRouter: Router = express.Router();
 
-recipeRouter.get("/", recipes_get);
-recipeRouter.get("/:id", recipe_by_id_get);
+recipeRouter.get("/", controller.getAllRecipes);
+recipeRouter.get("/:id", controller.getRecipeByRecipeId);
 recipeRouter.post(
   "/",
   recipeCreationValidation,
   handleRecipeErrors,
-  recipe_post,
+  controller.createRecipe,
 );
 
 export default recipeRouter;
