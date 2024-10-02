b30826a20fb27d9c6bdf4fd16637354fff694ada
aavetisyanIT <aavetisyanIT@gmail.com>
2024-10-02 16:42:26 -0500

    Update recipe router to use repository, interactor and controller classes

--- server/src/routes/recipeRouter.ts
+++ server/src/routes/recipeRouter.ts
@@ -1,18 +1,24 @@
 import express, { Router } from "express";
 
-import { controller } from "../controllers";
 import { recipeCreationValidation } from "../middleware/validators";
 import { handleRecipeErrors } from "../middleware/handleRecipeErrors";
+import { RecipeRepository } from "../repositories";
+import { RecipeInteractor } from "../interactors";
+import { RecipeController } from "../controllers";
+
+const repository = new RecipeRepository();
+const interactor = new RecipeInteractor(repository);
+const controller = new RecipeController(interactor);
 
 const recipeRouter: Router = express.Router();
 
-recipeRouter.get("/", controller.getAllRecipes);
-recipeRouter.get("/:id", controller.getRecipeByRecipeId);
+recipeRouter.get("/", controller.getAllRecipes.bind(controller));
+recipeRouter.get("/:id", controller.getRecipeByRecipeId.bind(controller));
 recipeRouter.post(
   "/",
   recipeCreationValidation,
   handleRecipeErrors,
-  controller.createRecipe,
+  controller.createRecipe.bind(controller),
 );
 
 export default recipeRouter;
