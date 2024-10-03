441cf5f20eb94bde3513fae903f8c3cee93d23cb
aavetisyanIT <aavetisyanIT@gmail.com>
2024-10-03 14:47:28 -0500

    test

--- server/src/config/redis.connection.ts
+++ server/src/config/redis.connection.ts
@@ -2,6 +2,8 @@ import { createClient } from "redis";
 
 import "dotenv/config";
 
+console.log("AAA process.env.REDIS_URL", process.env.REDIS_URL);
+
 export const redisClient = createClient({
   url: process.env.REDIS_URL || "redis://localhost:6379",
 });
