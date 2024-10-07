66f9a6d02fedc6794ebdf2c7bb06e658a8a546e8
aavetisyanIT <aavetisyanIT@gmail.com>
2024-10-07 14:00:19 -0500

    Define and implement getRecipeByRecipeId recipe repository method

--- server/src/interfaces/IRecipeRepository.ts
+++ server/src/interfaces/IRecipeRepository.ts
@@ -2,4 +2,8 @@ import { IRecipe } from "../models";
 
 export interface IRecipeRepository {
   getRecipesByUserId(userId: number): Promise<IRecipe[]>;
+  getRecipeByRecipeId(
+    recipeId: number,
+    userId: number,
+  ): Promise<IRecipe | null>;
 }
--- server/src/repositories/recipeRepository.ts
+++ server/src/repositories/recipeRepository.ts
@@ -3,7 +3,7 @@ import { Pool, QueryResult } from "pg";
 import { IRecipeRepository } from "../interfaces/IRecipeRepository";
 import { IRecipe } from "../models";
 import { pool } from "../config";
-import { selectAllRecipesByUserId } from "../db";
+import { selectAllRecipesByUserId, selectRecipeById } from "../db";
 
 export class RecipeRepository implements IRecipeRepository {
   private client: Pool;
@@ -15,5 +15,15 @@ export class RecipeRepository implements IRecipeRepository {
       selectAllRecipesByUserId(userId),
     );
     return userRecipes.rows[0];
+  }
+  async getRecipeByRecipeId(
+    recipeId: number,
+    userId: number,
+  ): Promise<IRecipe | null> {
+    const recipe: QueryResult<IRecipe> = await this.client.query(
+      selectRecipeById(recipeId, userId),
+    );
+    if (recipe.rowCount === 0) return null;
+    return recipe.rows[0];
   }
 }
