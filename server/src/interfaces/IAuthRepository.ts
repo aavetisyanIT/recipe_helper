34f5bddc4c2cccea03f4d11cebdf73ad9913d1ac
aavetisyanIT <aavetisyanIT@gmail.com>
2024-10-15 15:29:44 -0500

    Define auth repository class interface

--- server/src/interfaces/IAuthRepository.ts
+++ server/src/interfaces/IAuthRepository.ts
@@ -1,3 +1,25 @@
+import { IUser } from "../models";
+
 export interface IAuthRepository {
-  getAuth(): number;
+  selectUserByEmailAndUserName(
+    email: string,
+    userName: string,
+  ): Promise<IUser | null>;
+
+  hashNewUserPassword(password: string): Promise<string>;
+
+  createNewUser(
+    userName: string,
+    email: string,
+    hashedUserPassword: string,
+  ): Promise<IUser>;
+
+  generateToken(user: IUser): string;
+
+  cacheNewUser(
+    token: string,
+    id: number,
+    userName: string,
+    email: string,
+  ): void;
 }
