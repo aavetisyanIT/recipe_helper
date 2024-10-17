53b44322e525ea0ac715428f4732e29ad0507ef0
aavetisyanIT <aavetisyanIT@gmail.com>
2024-10-17 17:12:54 -0500

    Refactor app file and remove redundant code

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,8 @@
-import express, { Application, Response } from "express";
-import { QueryResult } from "pg";
+import express, { Application } from "express";
 import cookieParser from "cookie-parser";
-
-import { pool } from "./config/database.connection";
 import { requireAuth, rateLimiter, errorHandler } from "./middleware";
-import { IUser } from "./models";
 import authRouter from "./routes/authRouter";
 import recipeRouter from "./routes/recipeRouter";
-import { IAuthUserRequest } from "./types";
 
 const app: Application = express();
 app.use(express.json());
@@ -19,15 +14,5 @@ app.use(errorHandler);
 
 app.use("/auth", rateLimiter, authRouter);
 app.use("/recipes", rateLimiter, requireAuth, recipeRouter);
-
-app.get(
-  "/",
-  rateLimiter,
-  requireAuth,
-  async (req: IAuthUserRequest, res: Response) => {
-    const result: QueryResult<IUser> = await pool.query("SELECT * FROM users");
-    res.status(200).json(result.rows);
-  },
-);
 
 export default app;
